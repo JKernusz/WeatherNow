b3ccbb1860b5a9b36986b5a53e5f51f22106df3b
John Kay <jkernusz1346@gmail.com>
2023-11-06 20:47:43 -0500

    finished js

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# WeatherNow
-assignment for week 6
+# weatherNow
+Weather Application
+
+## Description
+The Weather Application is a simple web-based application that provides users with real-time weather information for a given location. Users can enter a city or a zip code and the application will display the current weather conditions, including temperature, humidity, wind speed, and a brief description of the weather.
+
+## Features
+User-friendly interface
+Real-time weather data
+Search functionality by city or zip code
+Display of current weather conditions (temperature, humidity, wind speed, weather description)
+Technologies Used
+HTML
+CSS
+JavaScript
+OpenWeatherMap API (or any other weather API you used)
+## Installation
+No installation required.
+Replace the API key in the code with your own API key. You can obtain an API key by signing up on the weather API provider's website.
+Save the changes.
+## Usage
+Open the index.html file in your preferred web browser.
+Enter a city name or a zip code in the search bar.
+Click the "Search" button or press Enter.
+The application will display the current weather conditions for the specified location.
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+## Acknowledgements
+OpenWeatherMap for providing the weather data API.
+Contributing
+Contributions are welcome! If you find any bugs or have suggestions for improvements, please open an issue or submit a pull request.
+
--- assets/script.js
+++ assets/script.js
@@ -2,33 +2,119 @@
 
 // user needs a searchbar and search button
 
-// add event listener for search button 
-var searchBar = $("#searchBar")
-var searchButton = $("#searchButton")
-var apiKey = "cb94559ccc9a2bc218cdf00db20e4853"
-function searchHandler(){
-    // get value of searchbar 
-    var cityName = searchBar.val()
-getWeather(cityName)
-// store all serched cities in local sotage
-
+// add event listener for search button
+var searchBar = $("#searchBar");
+var searchButton = $("#searchButton");
+var apiKey = "cb94559ccc9a2bc218cdf00db20e4853";
+var searchedCities = localStorage.getItem("cityName");
+var todaysWeather = $("#todays-weather");
+var futureweather = $("#future-weather");
+var timeDisplayEl = $(".time-display");
+var cityEl = $("#city-El");
+var tempEl = $("#temp-El");
+var humidityEl = $("#humidity-El");
+var windEl = $("#wind");
+var iconEl = $("#icon");
+var day1El = $("#day-1");
+var day2El = $("#day-2");
+var day3El = $("#day-3");
+var day4El = $("#day-4");
+var day5El = $("#day-5");
+function displayTime() {
+  var rightNow = dayjs().format("MM/DD/YYYY");
+  console.log(rightNow);
+  timeDisplayEl.text(rightNow);
 }
 
-function getWeather(city){
-    // call api and search for desired city
-var apiLink = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=" + apiKey + "&units=imperial"
-fetch(apiLink)
-.then(function(response){
-return response.json()
-})
-.then(function(data){
-    // display data on screen
-console.log(data)
-})
+function searchHandler() {
+  // get value of searchbar
+  var cityName = searchBar.val();
+  getWeather(cityName);
+  getWeatherFiveDays(cityName);
+  
+  // store all serched cities in local sotage
+  localStorage.setItem("cityName", cityName);
+}
+
+function getWeather(city) {
+  // call api and search for desired city
+  var apiLink =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    apiKey +
+    "&units=imperial";
+  fetch(apiLink)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // display data on screen
+      // var cityNameEl = ("div").data.name
+      cityEl.append(`City: ${data.name}`);
+      tempEl.append(`Temp: ${Math.round(data.main.temp)} \xB0 F`);
+      humidityEl.append(`Humidity: ${data.main.humidity}%`);
+      windEl.append(`Wind Speed: ${data.wind.speed} mph`);
+      iconEl.append(
+        `<img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" />`
+      );
+    });
 }
 
 
+function getWeatherFiveDays(city) {
+    var fiveDaysLink = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
 
+  fetch(fiveDaysLink)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // display data on screen
+      // var cityNameEl = ("div").data.name
+      console.log(data.list);
+      day1El.append(`Day 1 Temp: ${Math.round(data.list[3].main.temp)} \xB0 F  `);
+      day1El.append(`Humidity: ${data.list[3].main.humidity}%  `);
+      day1El.append(`Wind Speed: ${data.list[3].wind.speed} mph`);
+      day1El.append(
+        `<img src="http://openweathermap.org/img/w/${data.list[3].weather[0].icon}.png" />`
+      );
+   
+      //day 2 weather append
+      day2El.append(`Day 2 Temp: ${Math.round(data.list[11].main.temp)} \xB0 F  `);
+      day2El.append(`Humidity: ${data.list[11].main.humidity}%  `);
+      day2El.append(`Wind Speed: ${data.list[11].wind.speed} mph`);
+      day2El.append(
+        `<img src="http://openweathermap.org/img/w/${data.list[11].weather[0].icon}.png" />`
+      );
+    
+     //day 3 weather append
+      day3El.append(`Day 3 Temp: ${Math.round(data.list[19].main.temp)} \xB0 F  `);
+      day3El.append(`Humidity: ${data.list[19].main.humidity}%  `);
+      day3El.append(`Wind Speed: ${data.list[19].wind.speed} mph`);
+      day3El.append(
+        `<img src="http://openweathermap.org/img/w/${data.list[19].weather[0].icon}.png" />`
+      );
+    
+      //day 4 weather append
+      day4El.append(`Day 4 Temp: ${Math.round(data.list[27].main.temp)} \xB0 F  `);
+      day4El.append(`Humidity: ${data.list[27].main.humidity}%  `);
+      day4El.append(`Wind Speed: ${data.list[27].wind.speed} mph`);
+      day4El.append(
+        `<img src="http://openweathermap.org/img/w/${data.list[27].weather[0].icon}.png" />`
+      );
+    
+     //day 5 weather append
+      day5El.append(`Day 5 Temp: ${Math.round(data.list[35].main.temp)} \xB0 F  `);
+      day5El.append(`Humidity: ${data.list[35].main.humidity}%  `);
+      day5El.append(`Wind Speed: ${data.list[35].wind.speed} mph`);
+      day5El.append(
+        `<img src="http://openweathermap.org/img/w/${data.list[35].weather[0].icon}.png" />`
+      );
+    })
+      
+}
 
+displayTime();
 
-searchButton.on("click", searchHandler)
+searchButton.on("click", searchHandler);
